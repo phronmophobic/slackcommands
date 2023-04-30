5cbee1160cdded4185684d95c011c8c5a8fd2f51
Adrian Smith <smith.adriane@gmail.com>
2023-04-30 21:24:06 +0000

    Implement slack command portion.

--- src/slackcommands/ai.clj
+++ src/slackcommands/ai.clj
@@ -13,28 +13,84 @@
 
 (defn chat-command [request]
   ;; gpt-3.5-turbo
+  (let [text (get-in request [:form-params "text"])
+        response-url (get-in request [:form-params "response_url"])]
+
+    (when (seq (clojure.string/trim text))
+      (future
+        (let [response (api/create-chat-completion {:model "gpt-3.5-turbo"
+                                                    :messages [{:role "user" :content text}]}
+                                                   {:api-key api-key})
+              chat-response (-> response
+                                :choices
+                                first
+                                :message
+                                :content)]
+          (try
+            (client/post response-url
+                         {:body (json/write-str
+                                 {
+                                  "response_type" "in_channel",
+                                  "text" chat-response
+
+                                  "replace_original" "true"})
+                          :headers {"Content-type" "application/json"}})
+            (catch Exception e
+              (prn e))))))
+
+    {:body (json/write-str
+            {"response_type" "in_channel"
+             "blocks" [{"type" "section"
+                        #_#_"text" {"type" "plain_text"
+                                "emoji" true
+                                "text" ":waitingcat:"}}]}) 
+     :headers {"Content-type" "application/json"}
+     :status 200}
+
+
+    )
 )
 
 (defn image-command [request]
   (let [text (get-in request [:form-params "text"])
         response-url (get-in request [:form-params "response_url"])]
-    (future
-      (Thread/sleep 2000)
-      (try
-        (client/post response-url
-                     {:body (json/write-str
-                             {"text" "hello"
-                              "replace_original" true})
-                      :headers {"Content-type" "application/json"}})
-        (catch Exception e
-          (prn e))))
+
+    (when (seq (clojure.string/trim text))
+      (future
+        (let [response (api/create-image {:prompt text
+                                          :n 1
+                                          :size "512x512"}
+                                         {:api-key api-key})
+              image-url (-> response
+                            :data
+                            first
+                            :url)]
+
+          (try
+            (client/post response-url
+                         {:body (json/write-str
+                                 {
+                                  "response_type" "in_channel",
+                                  "blocks"
+                                  [{
+                                    "type" "image",
+                                    "image_url" image-url
+                                    "alt_text" text
+                                    ;; "text" image-url
+                                    }
+                                   ]
+
+                                  "replace_original" true})
+                          :headers {"Content-type" "application/json"}})
+            (catch Exception e
+              (prn e))))))
 
     {:body (json/write-str
             {"response_type" "in_channel"
              #_#_"blocks" [{"type" "section"
-                            "text" {"type" "plain_text"
-                                    "emoji" true
-                                    "text" ":waitingcat:"}}]}) 
+                        "text" {"type" "plain_text"
+                                "emoji" true
+                                "text" ":waitingcat:"}}]})
      :headers {"Content-type" "application/json"}
      :status 200}
 
@@ -43,10 +99,15 @@
 
 (comment
   (def response
+    
   (api/create-image {:prompt "An oracle visiting the real monsters tribe"
                      :n 5
                      :size "512x512"}
-                    {:api-key api-key}))
+                    {:api-key api-key})
+
+    (def response
+      )
+    ,)
 
 (def imgs
   (into []
@@ -62,10 +123,6 @@
   (skia/run #'debug)
   ,)
 
-#_(api/create-completion {:model "text-davinci-003"
-                          :prompt "Say this is a test"}
-                         {:api-key api-key
-                          ;;:organization "abcd"
-                          })
+
 
 
