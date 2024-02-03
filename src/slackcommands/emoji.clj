d6b5200cf9d0a23f419eb9f86bcd9ac95a9e1c4e
Adrian Smith <smith.adriane@gmail.com>
2024-02-03 00:07:11 +0000

    emoji->url works for both custom and builtin emoji.

--- src/slackcommands/emoji.clj
+++ src/slackcommands/emoji.clj
@@ -2,9 +2,10 @@
   (:require [clojure.data.json :as json]
             [clojure.string :as str]
             [slackcommands.util :as util]
+            [slackcommands.slack :as slack ]
             [membrane.skia :as skia]
             [membrane.ui :as ui]
-            ;; [membrane.skia.paragraph :as para]
+            [membrane.skia.paragraph :as para]
             [clojure.java.io :as io]))
 
 
@@ -38,26 +39,29 @@
     (emoji-table*)))
 
 (defn emoji->url [short-name]
-  (let [emoji (get @emoji-table short-name)
-        view
-        (ui/padding 30
-         (ui/label emoji
-                   (ui/font (.getCanonicalPath (io/file "NotoColorEmoji-Regular.ttf"))
-                            100)))
-        #_(para/paragraph
+  (if-let [emoji (get @emoji-table short-name)]
+    (let [view
+          #_(ui/label emoji
+                      (ui/font (.getCanonicalPath (io/file "NotoColorEmoji-Regular.ttf"))
+                               65))
+          (para/paragraph
            {:text emoji
             :style
-            #:text-style {:font-size 100
-                          :font-families ["/Users/adrian/Downloads/Noto_Color_Emoji/NotoColorEmoji-Regular.ttf"]}})
-              fname (str "emoji-" short-name ".png")
-        f (io/file
-           "aimages"
-           fname)]
-    (skia/save-image (.getCanonicalPath f)
-                     view
-                     nil
-                     nil
-                     100
-                     false)
-    (util/upload-file f)
-    (str "https://" "aimages.smith.rocks/" fname)))
+            #:text-style {:font-size 100}})
+          fname (str "emoji-" short-name ".png")
+          f (io/file
+             "aimages"
+             fname)]
+      (skia/save-image (.getCanonicalPath f)
+                       view
+                       nil
+                       nil
+                       100
+                       false)
+      (util/upload-file f)
+      (str "https://" "aimages.smith.rocks/" fname))
+    ;; else
+    (if-let [url (get (slack/list-emoji) (keyword short-name))]
+      url
+      (throw (ex-info "Emoji not found."
+                      {:emoji short-name})))))
